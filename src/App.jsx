04ee19e9d2fb39b3d684a0c71a22c3dc43def692
Seonghyeon Cho <seonghyeon.cho@wehealed.com>
2021-03-01 17:08:51 +0900

    Update up to 6.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,18 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    return await axios.get(
-      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json',
-      {
-        params: {
-          sort_by: 'rating',
-          order_by: 'desc',
-        },
-      }
-    );
-  };
-
-  async componentDidMount() {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await this.getMovies();
-
-    this.setState({ movies, isLoading: false });
-  }
-
-  renderMovies(movie) {
-    return (
-      <Movie
-        key={movie.id}
-        id={movie.id}
-        year={movie.year}
-        title={movie.title}
-        summary={movie.summary}
-        poster={movie.medium_cover_image}
-        genres={movie.genres}
-      />
-    );
-  }
-
-  render() {
-    const { movies, isLoading } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">{movies.map(this.renderMovies)}</div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>😃 Hello World!</span>
+      <span>- Seonghyeon, 2021</span>
+    </div>
+  );
+}
+
+export default About;
