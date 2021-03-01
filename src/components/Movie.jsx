de9babfb82405b96eb616ed3ed4d4453e5c6b6ed
Seonghyeon Cho <seonghyeon.cho@wehealed.com>
2021-03-01 17:54:03 +0900

    Update up to 6.4 (Done)

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Movie.css';
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genre">
-          {genres.map((genre, index) => (
-            <li className="genres__genre" key={index}>
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary}</p>
-      </div>
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genre">
+            {genres.map((genre, index) => (
+              <li className="genres__genre" key={index}>
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary}</p>
+        </div>
+      </Link>
     </div>
   );
 }
