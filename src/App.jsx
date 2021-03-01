944cb3fbf3bcef8f88ddd7c2ea290502f06a008a
Seonghyeon Cho <seonghyeon.cho@wehealed.com>
2021-03-01 15:52:58 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    return await axios.get(
+      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json',
+      {
+        params: {
+          sort_by: 'rating',
+          order_by: 'desc',
+        },
+      }
+    );
+  };
+
+  async componentDidMount() {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await this.getMovies();
+
+    this.setState({ movies, isLoading: false });
+  }
+
+  renderMovies(movie) {
+    return (
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+      />
+    );
+  }
+
+  render() {
+    const { movies, isLoading } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">{movies.map(this.renderMovies)}</div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Movie.css';
+
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genre">
+          {genres.map((genre, index) => (
+            <li className="genres__genre" key={index}>
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
